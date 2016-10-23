bcbc6b2ae826666b398a2b41b0080819a3329bda
greatsundaranand <greatsundaranand@gmail.com>
2016-10-23 16:31:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,10 @@ console.log('Loaded!');
 function fn1() {
     var mydiv=document.getElementById("tantan");
     mydiv.innerHTML="Thanks for clicking me";
+    setInterval(fn2,100);
+}
+
+function fn2() {
     var myimage=document.getElementById("myimage");
     if (myimage.style.marginleft==='') {
     myimage.style.marginleft="100px";
