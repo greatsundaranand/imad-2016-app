014609a6c69741b7fcb253b9e7ef9a1fff2f3db2
greatsundaranand <greatsundaranand@gmail.com>
2016-10-23 16:36:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ function fn1() {
 
 function fn2() {
     var myimage=document.getElementById("myimage");
-    mymargin=mymargin+100;
+    mymargin=mymargin+400;
     console.log(mymargin);
     myimage.style.marginleft=mymargin+"px";
 }
