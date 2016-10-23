a100a612db075c77c62fabe7fb84188bf0d31801
greatsundaranand <greatsundaranand@gmail.com>
2016-10-23 16:17:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
 function fn1() {
-    var mydiv=document.getElementbyId("tantan");
+    var mydiv=document.getElementById("tantan");
     mydiv.innerHTML="Thanks for clicking me";
 }
